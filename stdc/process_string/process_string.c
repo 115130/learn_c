50f554c750722f54165edaf689517ff352d1654d
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-10 11:40:00 +0800

    提交日期:2024_06_10_11_40_00

---
+++ stdc/process_string/process_string.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+//拷贝字符串
+char* str_cpy(char str,char str1);
+//连接字符串
+char* str_cat(char* str1,char str2);
+//比较字符串
+int str_cmp(char* str1,char* str2);
+//查找字符串
+int find_str(char* str,char* sub);
+//替换字符串
+char* replace_str(char* str,char* str1);
+//计算字符串长度
+int str_len(char* str);
+
+int main(void){
+    return 0;
+}
+//拷贝字符串
+char* str_cpy(char str,char str1){
+    return NULL;
+}
+//连接字符串
+char* str_cat(char* str1,char str2){
+    return NULL;
+}
+//比较字符串
+int str_cmp(char* str1,char* str2){
+    return 0;
+}
+//查找字符串
+int find_str(char* str,char* sub){
+    return 0;
+}
+//替换字符串
+char* replace_str(char* str,char* str1){
+    return NULL;
+}
+//计算字符串长度
+int str_len(char* str){
+    return 0;
+}

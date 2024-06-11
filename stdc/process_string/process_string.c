8913abdddd7e7e697c5eaef193f0e637f4ee2787
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-11 11:40:00 +0800

    提交日期:2024_06_11_11_40_00

--- stdc/process_string/process_string.c
+++ stdc/process_string/process_string.c
@@ -8,18 +8,20 @@ int str_cmp(char* str1,char* str2);
 //查找字符串
 int find_str(char* str,char* sub);
 //替换字符串
-char* replace_str(char* str,char* str1);
+char* replace_str(char*,char*,char*);
 //计算字符串长度
 unsigned long str_len(char* str);
 
 int main(void){
     unsigned long len = str_len("123456");
     printf("%lu\n",len);
-    char str[20] = "abcdefg";
-    char* str1="123456";
+    char str[20] = "123456";
+    char* str1="";
     printf("%s %s\n",str,str1);
     //str_cpy(str,str1);
-    str_cat(str,str1);
+    //str_cat(str,str1);
+    //printf("%d\n",str_cmp(str,str1));
+    //printf("%d\n",find_str(str,str1));
     printf("%s %s\n",str,str1);
     return 0;
 }
@@ -47,22 +49,46 @@ char* str_cat(char* str1,char* str2){
 int str_cmp(char* str1,char* str2){
     unsigned long str1_len = str_len(str1);
     unsigned long str2_len = str_len(str2);
-    for(unsigned long i = 0;i < str1_len;i++){
-        for(unsigned long j = 0;j < str2_len;j++){
-            if(str1[i] == str2[j]){
-                
-            }
-        }
+    long str1_v = 0;
+    long str2_v = 0;
+    for(int i = 0;i < str1_len;i++){
+        str1_v += *(str1 + i);
     }
-    return 0;
+    
+    for(int i = 0;i < str2_len;i++){
+        str2_v += *(str2 + i);
+    }
+    return str1_v - str2_v;
 }
 //查找字符串
-int find_str(char* str,char* sub){
-    return 0;
+int find_str(char* str1,char* str2){
+    unsigned long str1_len = str_len(str1);
+    unsigned long str2_len = str_len(str2);
+    int flag = 0;
+    for(unsigned long i = 0;i < str1_len;i++){
+        int c = 0;
+        for(unsigned long j = 0;j < str2_len;j++){
+            if(str1[i+j] == str2[j]){
+                c++;
+            }
+        }
+        if(c == str2_len && str2_len != 0){
+            flag++;
+            break;
+        }
+    }
+    return flag;
 }
 //替换字符串
-char* replace_str(char* str,char* str1){
-    return NULL;
+char* replace_str(char* source,char* str1,char* str2){
+    if(find_str(source,str1) == 1){
+        unsigned long str2_len = str_len(str2);
+        unsigned long str1_len = str_len(str1);
+
+        
+    }else{
+        return source;
+    }
 }
 //计算字符串长度
 unsigned long str_len(char* str){

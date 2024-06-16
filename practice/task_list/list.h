f3db23bbaf2df1eaaf4184d0968b328f0990d70a
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-16 20:40:01 +0800

    提交日期:2024_06_16_20_40_00

---
+++ practice/task_list/list.h
@@ -0,0 +1,25 @@
+
+
+#ifndef __LIST_H__
+#define __LIST_H__
+#ifndef __TASK_LIST_H__
+    #include "task_list.h"
+    #define __TASK_LIST_H__
+    #endif
+typedef struct node{
+    task_t task;
+    struct node* next;
+}node_t;
+
+typedef struct list{
+    node_t* head;
+    node_t* tail;
+}list_t;
+
+void list_init(list_t* list);
+void list_deinit(list_t* list);
+void list_add(list_t* list,node_t test);
+void list_head_add(list_t* list,node_t test);
+void printf_task(list_t* list);
+void list_del(list_t* list);
+#endif
--- practice/task_list/task_list.h
+++ practice/task_list/task_list.h
@@ -1,11 +1,20 @@
 #ifndef __TASK_LIST_H__
 #define __TASK_LIST_H__
+
+    #ifndef __LIST_H__
+    #define __LIST_H__
+    #include"list.h"
+    #endif
+
 typedef enum {
-    
+    FINISHED,
+    UNFINISHED,
 }task_status_t; 
 
 typedef struct task{
-    char* tsak;
+    char* task;
     task_status_t task_status;
 }task_t;
 #endif
+
+

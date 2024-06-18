1465f53b78be498312f047e245666bee434f3a48
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-18 08:40:02 +0800

    提交日期:2024_06_18_08_40_00

--- practice/task_list/list.c
+++ practice/task_list/list.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#pragma  once
+#include <string.h>
 #include"list.h"
 #include "task_list.h"
 void list_init(list_t* list){
@@ -32,4 +32,41 @@ void list_head_add(list_t* list,task_t task){
     head->next = node_new;
     node_new->next = mid;
 }
+void list_tail_add(list_t* list,task_t task){
+    node_t* node_new = malloc(sizeof(node_t));
+    node_new->task = task;
+    node_t* first  = list->head;
+    node_t* mid  = first->next;
+    while(mid->next!=NULL){
+        first= mid;
+        mid = mid->next;
+    }
+    first->next = node_new;
+    node_new->next = mid;
 
+}
+
+void list_del(list_t* list,task_t task){
+    node_t* first  = list->head;
+    node_t* mid  = first->next;
+
+    while(strcmp(task.task,mid->task.task)&&task.task_status == mid->task.task_status && mid->next!=NULL){
+        first = mid;
+        mid = mid->next;
+    }
+    first->next = mid->next;
+    if(mid->next!=NULL){
+        free(mid);
+    } 
+}
+void printf_task(list_t* list){
+    node_t* first  = list->head;
+    node_t* mid  = first->next;
+
+    while(mid->next!=NULL){
+        first = mid;
+        printf("\t%s\t%s",mid->task.task,mid->task.task_status==FINISHED?"已完成":"未完成");
+        mid = mid->next;
+    }
+
+}

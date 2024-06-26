1b30869f1c8da72520d87890d37a8974aa853d80
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-26 18:00:02 +0800

    提交日期:2024_06_26_18_00_00

--- unixc/day20/join.c
+++ unixc/day20/join.c
@@ -2,27 +2,44 @@
 #include<unistd.h>
 #include<pthread.h>
 #include<string.h>
+#include<stdlib.h>
 #define PI 3.14
 
-//double c =  0;
 
 void* area(void* arg){
     double r = *(double*)arg;
-    static double t = 0;
-    t = (PI*r*r);
-    return (void*)&t;
-    
+    double *t = malloc(sizeof(double));
+    *t = (PI*r*r);
+    return (void*)t;
 }
 
 
 int main(void){
     double r =10;
     pthread_t tid;
-    tid = pthread_create(&tid,NULL,area,&r);
-    double *t;
+    pthread_create(&tid,NULL,area,&r);
+    if( tid == -1){
+        perror("pthread_create");
+        return -1;
+    }
+    double *t = NULL;
+   
+    double r2 =20;
+    pthread_t tid2;
+    pthread_create(&tid2,NULL,area,&r2);
+    if(tid2 == -1){
+        perror("pthread_create");
+        return -1;
+    }
+    double *t2 = NULL; 
+
+
     pthread_join(tid,(void**)&t);
+    printf("圆的面积是%lg\n",*t);
 
-    printf("圆的面积是%lg\n",r);
-
+    pthread_join(tid2,(void**)&t2);
+    printf("圆的面积是%lg\n",*t2);
+    free(t);
+    free(t2);
     return 0;
 }

ffe6036f02a0055f9437f9e02493374c574f862a
jiahui115 <wodeqiyuxin@gmail.com>
2024-06-27 21:01:44 +0800

    提交日期:2024_06_27_21_00_00

--- unixc/day20/https.c
+++ unixc/day20/https.c
@@ -4,8 +4,25 @@
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<arpa/inet.h>
+#include <ctype.h>
+
+void urlencode_utf8(const unsigned char *src, char *dest) {
+    const char *hex = "0123456789ABCDEF";
+    while (*src) {
+        if (isalnum(*src) || *src == '-' || *src == '_' || *src == '.' || *src == '~') {
+            *dest++ = *src;
+        } else {
+            *dest++ = '%';
+            *dest++ = hex[*src >> 4];
+            *dest++ = hex[*src & 15];
+        }
+        src++;
+    }
+    *dest = '\0';  // Null-terminate the destination string
+}
+
 int main(void){
-    char city[64] = {};
+    const unsigned char city[64]  = "武汉";
     const char* key ="05ec0972bd980656ada59c3d6cacb0d2";
     int sockfd =socket(AF_INET,SOCK_STREAM,0);
     if(sockfd ==-1){
@@ -15,21 +32,25 @@ int main(void){
     struct sockaddr_in ser;
     ser.sin_family=AF_INET;
     ser.sin_port=htons(80);
-    ser.sin_addr.s_addr=inet_addr("183.2.172.42");
+    ser.sin_addr.s_addr=inet_addr("203.107.54.210");
     if(connect(sockfd,(struct sockaddr*)&ser,sizeof(ser))==-1){
         perror("connect");
         return -1;
     }
-    printf("请从键盘输入你所在的城市");
-    scanf("%s",city);
+    //printf("请从键盘输入你所在的城市");
+    //scanf("%s",city);
+
+    char encode[64] = {};
+    urlencode_utf8(city,encode);
 
     char request[1024] = {};
-    sprintf(request,"GET / HTTP/1.1\r\n"
+    sprintf(request,"GET /simpleWeather/query?key=%s&city=%s HTTP/1.1\r\n"
                     "Host: apis.juhe.cn\r\n"
                     "Accept: */*\r\n"
-                    "Referer: http://apis.juhe.cn/simpleWeather/query?city=%s,key=%s\r\n"
                     "Content-Type: application/x-www-form-urlencoded\r\n"
-                    "Connection: close\r\n\r\n",city,key);
+                    "Connection: close\r\n\r\n",key,encode);
+
+    printf("%s",request);
     if(send(sockfd,request,strlen(request),0) == -1){
         perror("send");
         return -1;

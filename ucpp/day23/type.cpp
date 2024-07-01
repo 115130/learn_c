c26e11b589dfec48277aa53ffb006bd683454399
jiahui115 <wodeqiyuxin@gmail.com>
2024-07-01 15:00:40 +0800

    提交日期:2024_07_01_15_00_00

---
+++ ucpp/day23/type.cpp
@@ -0,0 +1,50 @@
+#include <ios>
+#include<iostream>
+#include <cstring>
+#include <ostream>
+
+using namespace std;
+void testStruct() {
+    struct Student {
+        int m_age;
+        char m_name[256];
+        void getinfo() {
+            cout << m_name << ":" << m_age << endl;
+        }
+    };
+    Student s;
+    s.m_age = 22;
+    strcpy(s.m_name,"1234");
+    cout<<"姓名" << s.m_name << "年龄" << s.m_age << endl;
+    s.getinfo();
+}
+
+void testUnion(){
+    union {
+        int i;
+        char c[4];
+    };
+    i = 0x12345678;
+    cout<< hex <<(int)c[0]<< ' '<<(int)c[1]<<' '<<(int)c[2]<<' '<<(int)c[3]<<endl;
+}
+
+void testEnum(){
+    enum Color{red,green,bule};
+    Color c = red;
+    cout<<red<<endl;
+    
+}
+
+void testBool(){
+    bool a = 0.0000000003 - 0.0000000003;
+    bool b =false;
+    cout << boolalpha << "a=" << a <<",b="<<b<<endl;
+}
+
+int main(){
+//    testStruct();
+//    testUnion();
+//    testEnum();
+    testBool();
+    return 0;
+}

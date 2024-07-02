14e5bacdda669de3de08b395a2fe370581aef603
jiahui115 <wodeqiyuxin@gmail.com>
2024-07-02 11:01:01 +0800

    提交日期:2024_07_02_11_00_00

--- ucpp/day02/new.cpp
+++ ucpp/day02/new.cpp
@@ -10,9 +10,19 @@ int main( void ){
     free(a);
     int* na = new int;
     cout << "na:"<<*na<<endl;
+    delete na;
     int* nan = new int(100);
     cout << "nan:"<<*nan<<endl;
+    delete nan;
+    
     int* naal = new int[7]{0};
     cout << "naal:"<<*naal<<endl;
+    delete[] naal;
+
+    int (*ta)[4] = new int[3][4];
+    delete[] ta;
+
+    int* arr = new int[0xFFFFFFFFFF];
+    
     return 0;
 }
